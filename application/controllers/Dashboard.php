0f88db49bfcbbae4d260d07081ed0ce4eccff0a7
Alexander <cchirito@nfocostudios.com>
2018-10-30 15:03:04 -0600

    Comentarios eliminados

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -4,6 +4,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Dashboard extends CI_Controller {
 
 	public function index() {
+		$this->load->view('templates/header_view');
 		$this->load->view('dashboard/dashboard_view');
+		$this->load->view('templates/footer_view');
 	}
 }
